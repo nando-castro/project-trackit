23676c8294c9d11c5730fb59b6893fdad661f886
nando-castro <fernando.castro10.master@gmail.com>
2022-05-27 20:13:15 -0300

    Screen Login and Register layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './../assets/css/reset.css';
+import './../assets/css/style.css';
+import Login from "./home/Login";
+import Register from "./home/Register";
+
 export default function App() {
-    return(
-        <p>Eu sou o APP</p>
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/cadastro" element={<Register />} />
+                {/* <Route path="/habitos" element={<TelaHabitos />} />
+                <Route path="/hoje" element={<TelaHoje />} />
+                <Route path="/historico" element={<TelaHistorico />} /> */}
+            </Routes>
+        </BrowserRouter>
     );
 }
---
+++ src/components/home/Login.js
@@ -0,0 +1,78 @@
+import styled from 'styled-components';
+import logo from '../../assets/img/logo.png';
+
+function Login() {
+    return (
+        <Home>
+            <img src={logo} alt="TrackIt" />
+            <input placeholder='email' />
+            <input placeholder='senha' />
+            <div>Entrar</div>
+            <p>Não tem uma conta? Cadastre-se!</p>
+        </Home>
+    );
+}
+
+const Home = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    background: #FFFFFF;
+
+    img {
+        width: 180px;
+        height: 178.38px;
+        margin-bottom: 32px;
+    }
+
+    input {
+        width: 80%;
+        height: 45px;
+        background: #FFFFFF;
+        border: 1px solid #D5D5D5;
+        border-radius: 5px;
+        margin-bottom: 6px;
+
+        font-family: 'Lexend Deca';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 19.976px;
+        line-height: 25px;
+        /* identical to box height */
+        color: #DBDBDB;
+    }
+
+    div {
+        width: 80%;
+        heigth: 45px;
+        padding: 10px;
+        background: #52B6FF;
+        border-radius: 4px;
+
+        font-family: 'Lexend Deca';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 26px;
+        text-align: center;
+
+        color: #FFFFFF;
+        margin-bottom: 25px;
+    }
+
+    p {
+        font-family: 'Lexend Deca';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 13.976px;
+        line-height: 17px;
+        text-align: center;
+        text-decoration-line: underline;
+        color: #52B6FF;
+    }
+`
+
+export default Login;
