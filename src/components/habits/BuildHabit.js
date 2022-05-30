fd45074239e885d25112c23286dbb91a002958bf
nando-castro <fernando.castro10.master@gmail.com>
2022-05-29 23:18:51 -0300

    Habits finish

---
+++ src/components/habits/BuildHabit.js
@@ -0,0 +1,135 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { useAuth } from '../../Providers/auth';
+import WeekDay from '../WeekDay';
+import { Container, Days, Botoes, Cancel, Save } from './style';
+import Loader from "react-loader-spinner";
+import { ToastContainer, toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+
+export default function BildHabit({ setCreatingHabit }) {
+
+    const [daysSelected, setDaysSelected] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [habit, setHabit] = useState('');
+    const { user } = useAuth();
+    const daysOfWeek = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+    const daysOfWeekNumber = [7, 1, 2, 3, 4, 5, 6];
+    let selected = false;
+
+    function cancelBild() {
+        setCreatingHabit(false);
+        setDaysSelected([]);
+    }
+
+    function changeDays(day) {
+        let arr = daysSelected;
+        if (daysSelected.includes(day)) {
+            arr.splice(daysSelected.indexOf(day), 1);
+            setDaysSelected([...arr]);
+        } else { setDaysSelected([...daysSelected, day]) }
+    }
+
+    function checkData() {
+        if (habit === '') {
+            toast.warn('Insira um hábito!');
+        } else if (daysSelected.length === 0) {
+            toast.warn('Selecione pelo menos um dia da semana!')
+        } else { CreateHabit(); }
+    }
+
+    function CreateHabit() {
+        setLoading(true);
+        console.log(habit, daysSelected.sort(), user.token)
+        const promise = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits',
+            { name: habit, days: daysSelected.sort() },
+            { headers: { 'Authorization': `Bearer ${user.token}` } }
+        )
+        promise.then(response => {
+            setCreatingHabit(false);
+            setDaysSelected([]);
+            setLoading(false);
+            window.location.reload();
+        })
+        promise.catch(err => setLoading(false))
+    }
+
+    return (
+        <Container>
+            <div>
+                <input disabled={loading} placeholder="Nome do hábito" type="text" onChange={e => setHabit(e.target.value)} value={habit} />
+
+                <Days>
+                    {daysOfWeek.map((day, index) => {
+                        const number = daysOfWeekNumber[index];
+                        daysSelected.includes(number) ? selected = true : selected = false;
+
+                        return <WeekDay loading={loading} key={index} number={number} text={day} changeDays={changeDays} selected={selected} />
+                    })}
+                </Days>
+            </div>
+
+            <Botoes>
+                <Cancel onClick={cancelBild} loading={loading} disabled={loading}>Cancelar</Cancel>
+
+                <Save loading={loading} onClick={checkData} disabled={loading}>
+                    {loading ? <Loader type="ThreeDots" color="white" width='50px' /> : 'Salvar'}
+                </Save>
+            </Botoes>
+            <ToastContainer limit={1} />
+        </Container>
+    )
+}
+
+import styled from "styled-components";
+
+export const Container = styled.div`
+height: 180px;
+padding: 18px;
+margin: 0 18px 30px;
+background-color: #fff;
+border-radius: 5px;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+input{
+    width: 100%;
+    height: 45px;
+    border-radius: 5px;
+}
+`
+export const Days = styled.div`
+display: flex;
+gap: 8px;
+`
+export const Botoes = styled.div`
+display: flex;
+justify-content: end;
+align-items: center;
+gap: 20px;
+`
+export const Cancel = styled.div`
+font-size: 15.976px;
+line-height: 20px;
+color: #52B6FF;
+text-align: center;
+${props => props.loading && 'opacity: 0.5;'}
+&:hover{
+    cursor: pointer;
+}
+`
+
+export const Save = styled.div`
+width: 84px;
+height: 35px;
+background: #52B6FF;
+color: #fff;
+border-radius: 4.63636px;
+${props => props.loading && 'opacity: 0.5;'}
+display: flex;
+justify-content: center;
+align-items: center;
+&:hover{
+    cursor: pointer;
+}
+`
--- src/components/today/Today.js
+++ src/components/today/Today.js
@@ -1,30 +1,49 @@
-import styled from "styled-components";
-
 import Header from "../Header";
 import Menu from "../Menu";
 import Title from "../Title";
 import Task from "../Task";
-
+import { Container, Tasks } from "./style";
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import { useAuth } from "../../Providers/auth";
+import axios from "axios";
 
 
 export default function PageToday() {
+
+    const daysOfWeek = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
+    const { user } = useAuth();
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        if (user) {
+            const promise = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/today',
+                { headers: { 'Authorization': `Bearer ${user.token}` } }
+            );
+            promise.then(response => {
+                setTasks(response.data);
+            })
+        }
+    }, [user])
+
     return (
         <Container>
             <Header />
-            <Title text={"Segunda-feira, 17/05"} description={'Nenhum Hábito concluido ainda'} />
+            <Title text={`${daysOfWeek[dayjs().day()]}, ${dayjs().format('DD/MM')}`} description={'Nenhum Hábito concluido ainda'} />
             <Tasks>
-                <Task />
-                <Task />
-                <Task />
-                <Task />
+                {tasks.length !== 0 && (
+                    tasks.map(task => <Task key={task.id} task={task} />)
+                )}
             </Tasks>
             <Menu />
         </ Container>
     )
 }
 
+
 const Container = styled.div`
     width: 100vw;
+    min-height: 100vh;
     padding: 75px 0px;
     background: #E5E5E5;
 `
